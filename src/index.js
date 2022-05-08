52ed9299ab8f92db8e4cca2e14f08747565c469d
Hans Yu <hans.yu@outlook.de>
2022-05-09 00:08:06 +0200

    Turn function into method.

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ class Board extends React.Component {
   render() {
     let status;
 
-    const winner = calculateWinner(this.state.squares);
+    const winner = this.calculateWinner();
     if (winner) {
       status = `Winner: ${winner}`;
     } else {
@@ -78,6 +78,27 @@ class Board extends React.Component {
       </div>
     );
   }
+
+  calculateWinner() {
+    const squares = this.state.squares;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
 
 class Game extends React.Component {
@@ -100,24 +121,4 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
